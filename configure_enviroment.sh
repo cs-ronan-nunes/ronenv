917e5f7664c9ff6cf3d0f0baaaee30160ca81c0c
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-10-09 17:50:53 -0300

    Delete configure_enviroment.sh

--- configure_enviroment.sh
+++
@@ -1,7 +0,0 @@
-echo '---'
-echo 'Configurando ZSH'
-echo '---'
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-cp zshrc ~/.zshrc
-chsh -s /bin/zsh
-echo 'eval "$(rbenv init -)"' >> ~/.zshrc
